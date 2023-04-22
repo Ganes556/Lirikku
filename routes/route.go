3dc2fa634d486c09aad7e2ad6ed2831eec85dc8d
Ganes556 <gustiganes@gmail.com>
2023-04-22 22:55:04 +0800

    add route auth for register & midd for route slash

---
+++ routes/route.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/Lirikku/controllers"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+
+func NewRoute() *echo.Echo{
+	e := echo.New()
+
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// auth
+	authGroup := e.Group("/auth")
+	authGroup.POST("/register", controllers.Register)
+
+	return e
+}
