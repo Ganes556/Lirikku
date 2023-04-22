0f41208b815cd864413e7453c08f27c78244cddc
Ganes556 <gustiganes@gmail.com>
2023-04-22 22:50:45 +0800

    add user model for register controller
    add user model for register controller:
    - ReqAuthUser user model for request body in register
    - hooks BeforeCreate gorm for hashing password

--- models/user_model.go
+++ models/user_model.go
@@ -1,13 +1,25 @@
 package models
 
 import (
+	"github.com/Lirikku/utils"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	gorm.Model `json:"-"`
 	Name 	 string `json:"name" gorm:"type:varchar(150)"`
 	Email 	string `json:"email" gorm:"type:varchar(255)"`
 	Password string `json:"password" gorm:"type:varchar(64)"`
 	Songs 	[]SongLyric `json:"songs" gorm:"foreignKey:UserID"`
+}
+
+type ReqAuthUser struct {
+	Name 	 string `json:"name"`
+	Email 	string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.Password = utils.HashPassword(u.Password)
+	return
 }
