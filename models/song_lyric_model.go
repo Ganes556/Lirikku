dc38caf70f681247261876333ee60909e5625555
Ganes556 <gustiganes@gmail.com>
2023-04-26 12:16:52 +0800

    add save model & prop base

--- models/song_lyric_model.go
+++ models/song_lyric_model.go
@@ -1,19 +1,30 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type SongLyric struct {
-	gorm.Model `json:"-"`
-	UserID 	 uint `json:"user_id" gorm:"type:unsignedInteger"`
+	Base
+	UserID  uint      `json:"user_id" gorm:"type:unsignedInteger"`
 	Artists []*Artist `json:"artists" gorm:"many2many:song_artists;"`
-	Title  string `json:"title" gorm:"type:varchar(150)"`
-	Lyric  string `json:"lyric"`
+	Title   string    `json:"title" gorm:"type:varchar(150)"`
+	Lyric   string    `json:"lyric"`
 }
 
-type ReqSongLyric struct {
-	Title  string `json:"title"`
-	Lyric  string `json:"lyric"`
+type SaveSongLyric struct {
+	Title   string   `json:"title"`
+	Lyric   string   `json:"lyric"`
 	Artists []string `json:"artists"`
+}
+
+func (s *SaveSongLyric) Convert2SongLyric(userID uint) *SongLyric {
+	var songLyric SongLyric
+	songLyric.UserID = userID
+	songLyric.Title = s.Title
+	songLyric.Lyric = s.Lyric
+
+	for _, artist := range s.Artists {
+		var NewArtist Artist
+		NewArtist.Name = artist
+		songLyric.Artists = append(songLyric.Artists, &NewArtist)
+	}
+
+	return &songLyric
 }
