bdd56de0096bac8f51402feb6ad2d70e32ae430f
Ganes556 <gustiganes@gmail.com>
2023-04-29 14:03:15 +0800

    refactor: controller my_song_lyrics
    refactor: controller my_song_lyrics:
    - make query to fit with new model

--- controllers/my_song_lyrics_controller.go
+++ controllers/my_song_lyrics_controller.go
@@ -6,24 +6,16 @@ import (
 	"github.com/Lirikku/configs"
 	"github.com/Lirikku/models"
 	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
 )
 
 func GetMySongLyrics(c echo.Context) error {
 
 	user := c.Get("user").(models.UserJWTDecode)
-	var songLyrics []models.SongLyric
-	
-	// load all song lyrics with artist
-	configs.DB.Preload("Artists", func(tx *gorm.DB) *gorm.DB{
-		return tx.Omit("created_at,deleted_at,updated_at")
-	}).Omit("user_id,created_at,deleted_at,updated_at").Where("user_id = ?", user.ID).Find(&songLyrics)
 	
 	var resSongLyrics []models.ResponseSongLyric
-
-	for _, songLyric := range songLyrics {
-		resSongLyrics = append(resSongLyrics, songLyric.Convert2ResSongLyric())
-	}
+	
+	// load all song lyrics with artist
+	configs.DB.Model(&models.SongLyric{}).Find(&resSongLyrics, "user_id = ?", user.ID)
 	
 	return c.JSON(http.StatusOK, echo.Map{
 		"my_song_lyrics": resSongLyrics,
@@ -33,14 +25,12 @@ func GetMySongLyrics(c echo.Context) error {
 func GetMySongLyric(c echo.Context) error {
 
 	user := c.Get("user").(models.UserJWTDecode)
-	var songLyric models.SongLyric
+	var resSongLyric models.ResponseSongLyric
 	
 	idSongLyric := c.Param("id")
 
 	// load song lyrics with artist by id song lyrics
-	err := configs.DB.Preload("Artists", func(tx *gorm.DB) *gorm.DB{
-		return tx.Omit("created_at,deleted_at,updated_at")
-	}).Omit("user_id,created_at,deleted_at,updated_at").Where("id = ? AND user_id = ?", idSongLyric, user.ID).First(&songLyric).Error
+	err := configs.DB.Model(&models.SongLyric{}).First(&resSongLyric, "id = ? AND user_id = ?", idSongLyric ,user.ID).Error
 	
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
@@ -49,7 +39,7 @@ func GetMySongLyric(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, echo.Map{
-		"my_song_lyrics": songLyric.Convert2ResSongLyric(),
+		"my_song_lyrics": resSongLyric,
 	})
 }
 
@@ -57,13 +47,13 @@ func SaveMySongLyric(c echo.Context) error {
 
 	user := c.Get("user").(models.UserJWTDecode)
 	
-	var songLyric models.WriteSongLyric
+	var newSongLyric models.SongLyric
 
-	c.Bind(&songLyric)
+	c.Bind(&newSongLyric)
 
-	var newSongLyric = songLyric.Convert2SongLyric(user.ID)
+	newSongLyric.UserID = user.ID
 
-	err := configs.DB.Create(newSongLyric).Error
+	err := configs.DB.Create(&newSongLyric).Error
 
 	if err != nil {
 		return err
@@ -80,24 +70,16 @@ func SearchMySongLyric(c echo.Context) error {
 	user := c.Get("user").(models.UserJWTDecode)
 	
 	
-	var songLyrics []models.SongLyric
+	var resSongLyrics []models.ResponseSongLyric
 	
 	title := c.QueryParam("title")
 	lyric := c.QueryParam("lyric")
-	artists:= c.QueryParam("artists")	
-
-	querySql := `SELECT sl.*, a.* FROM song_lyric_artists sla 
-					JOIN song_lyrics sl ON sl.id = sla.song_lyric_id 
-					JOIN artists a ON a.id = sla.artist_id
-					WHERE sl.user_id = ? 
-					AND sl.title LIKE ?
-					AND sl.lyric LIKE ?
-					AND a.name LIKE ?`
-
-	configs.DB.Raw(querySql, user.ID, "%"+title+"%", "%"+lyric+"%","%"+artists+"%").Preload("Artists").Find(&songLyrics)
+	artist_names:= c.QueryParam("artist_names")
+	
+	configs.DB.Model(&models.SongLyric{}).Where("user_id = ? AND title LIKE ? AND lyric LIKE ? AND artist_names LIKE ?", user.ID, "%"+title+"%", "%"+lyric+"%", "%"+artist_names+"%").Find(&resSongLyrics)
 
 	return c.JSON(http.StatusOK, echo.Map{
-		"my_song_lyrics": songLyrics,
+		"my_song_lyrics": resSongLyrics,
 	})
 }
 
@@ -107,17 +89,15 @@ func DeleteMySongLyric(c echo.Context) error {
 	
 	idSongLyric := c.Param("id")
 
-	err := configs.DB.First(&models.SongLyric{}, "id = ? AND user_id = ?", idSongLyric, user.ID).Error
+	querySongLyric := models.SongLyric{}
+
+	err := configs.DB.First(&querySongLyric, "id = ? AND user_id = ?", idSongLyric, user.ID).Error
 	
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
 			"message": "song lyric not found",
 		})
 	}
-
-	var querySongLyric *models.SongLyric
-	
-	configs.DB.Where("id = ? AND user_id = ?", idSongLyric, user.ID).Preload("Artists").First(&querySongLyric)
 
 	err = configs.DB.Unscoped().Delete(&querySongLyric).Error	
 
@@ -136,8 +116,8 @@ func UpdateMySongLyric(c echo.Context) error {
 	user := c.Get("user").(models.UserJWTDecode)
 	
 	idSongLyric := c.Param("id")
-
-	err := configs.DB.Omit("created_at,deleted_at,updated_at").First(&models.SongLyric{}, "id = ? AND user_id = ?", idSongLyric, user.ID).Error
+	
+	err := configs.DB.First(&models.SongLyric{}, "id = ? AND user_id = ?", idSongLyric, user.ID).Error
 	
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
@@ -145,13 +125,13 @@ func UpdateMySongLyric(c echo.Context) error {
 		})
 	}
 
-	var updateSongLyric models.WriteSongLyric
+	var updateSongLyric models.SongLyric
 
 	c.Bind(&updateSongLyric)
 
-	newUpdatedSongLyric := updateSongLyric.Convert2SongLyric(user.ID)
+	updateSongLyric.UserID = user.ID
 
-	err = configs.DB.Where("id = ?", idSongLyric).Updates(&newUpdatedSongLyric).Error
+	err = configs.DB.Where("id = ?", idSongLyric).Updates(&updateSongLyric).Error
 
 	if err != nil {
 		return err
