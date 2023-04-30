abdbed00a50e38db462b8f0f8b66ee17ad9276aa
Ganes556 <gustiganes@gmail.com>
2023-04-30 20:38:58 +0800

    feat: add middleware verify token JWT & set user data

---
+++ middlewares/jwt_middleware.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Lirikku/models"
+	"github.com/golang-jwt/jwt/v4"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func JwtMiddleware() echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(models.JWTClaims)
+		},
+		ParseTokenFunc: func(c echo.Context, auth string) (interface{}, error) {
+			return jwt.ParseWithClaims(auth, new(models.JWTClaims), func(token *jwt.Token) (interface{}, error) {
+				if token.Claims.(*models.JWTClaims).ExpiresAt.Time.Before(time.Now()) {
+					return nil, fmt.Errorf("token expired")
+				}
+				return []byte(os.Getenv("JWT_SECRET")), nil
+			})
+		},
+		SuccessHandler: func(c echo.Context) {
+			data := c.Get("user").(*jwt.Token).Claims.(*models.JWTClaims)
+			c.Set("user", models.UserJWTDecode{
+				ID: data.ID,
+				Name: data.Name,
+			})
+		},
+		ErrorHandler: func(c echo.Context, err error) error {
+			if err.Error() != "token expired" {
+				return echo.NewHTTPError(http.StatusUnauthorized, "invalid token")
+			}
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+		},
+	})
+}
