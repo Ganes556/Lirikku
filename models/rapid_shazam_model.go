bb9bf60c74a98806de80f5a99170cc0b4d5b7611
Ganes556 <gustiganes@gmail.com>
2023-05-01 21:06:46 +0800

    fix: logic request search by audio

--- models/rapid_shazam_model.go
+++ models/rapid_shazam_model.go
@@ -1,7 +1,30 @@
 package models
 
+import "strings"
+
 type ResponseRapidShazamSearchAudio struct {
 	Track struct {
-		Key string `json:"key"`
+		Key      string `json:"key"`
+		Title    string `json:"title"`
+		Subtitle string `json:"subtitle"`
+		Sections []struct {
+			Text []string `json:"text,omitempty"`
+		} `json:"sections"`
 	} `json:"track"`
+}
+
+func (sa *ResponseRapidShazamSearchAudio) GetLyrics() string {
+	var lyrics string
+	if len(sa.Track.Sections) > 1 {
+		lyrics = strings.Join(sa.Track.Sections[1].Text, "\n")
+	}
+	return lyrics
+}
+
+func (sa *ResponseRapidShazamSearchAudio) GetInResponsePublicSongLyric() ResponsePublicSongLyric {
+	var res ResponsePublicSongLyric
+	res.Title = sa.Track.Title
+	res.ArtistNames = sa.Track.Subtitle
+	res.Lyric = sa.GetLyrics()
+	return res
 }
