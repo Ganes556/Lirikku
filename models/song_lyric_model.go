73e69d174e99530ab27ecef28449a977eb53fd2e
Ganes556 <gustiganes@gmail.com>
2023-05-01 10:52:04 +0800

    feat: add shazam api & public song lyric models

--- models/song_lyric_model.go
+++ models/song_lyric_model.go
@@ -17,6 +17,13 @@ type ResponseSongLyric struct {
 	Lyric   string   `json:"lyric"`
 }
 
+type ResponsePublicSongLyric struct {
+	ArtistNames string `json:"artist_names"`
+	Title   string   `json:"title"`
+	Lyric   string   `json:"lyric"`
+}
+
+
 // hooks
 func (s *SongLyric) AfterDelete(tx *gorm.DB) (err error) {
 	tx.Exec("ALTER TABLE song_lyrics AUTO_INCREMENT = 1")
