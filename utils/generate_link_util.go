aa04d2564cc67ab2dcb7dbf799ece0e0de39d586
Ganes556 <gustiganes@gmail.com>
2023-05-01 11:02:53 +0800

    fix: logic generate next link

--- utils/generate_link_util.go
+++ utils/generate_link_util.go
@@ -1,17 +1,15 @@
 package utils
 
 import (
-	"strconv"
-
 	"github.com/labstack/echo/v4"
 )
 
-func GenerateNextLink(c echo.Context, offset, lenSongLyrics int) string {
+func GenerateNextLink(c echo.Context, lenData int, param string) string {
 	var next string
-	if lenSongLyrics < 5 {
+	if lenData < 5 {
 		next = ""
 	} else {
-		next = "http://"+ c.Request().Host + c.Request().URL.Path + "?offset=" + strconv.Itoa(offset + 5)
+		next = "http://"+ c.Request().Host + c.Request().URL.Path + "?" + param
 	}
 	
 	return next
