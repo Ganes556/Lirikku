e8388ee63aa547a5122533d4b379746308cc8e74
Ganes556 <gustiganes@gmail.com>
2023-05-01 19:44:26 +0800

    feat: add search song lyrics by audio

--- controllers/public_song_lyrics_controller.go
+++ controllers/public_song_lyrics_controller.go
@@ -66,5 +66,59 @@ func SearchTermSongLyrics(c echo.Context) error {
 }
 
 func SearchAudioSongLyrics(c echo.Context) error {
-	return nil
+	audioData, err := c.FormFile("audio")
+	
+	if err != nil {
+		return err
+	}
+
+	if audioData.Size > 500000 {
+		return c.JSON(http.StatusRequestEntityTooLarge, echo.Map{
+			"message": "audio size must be less than 500kb",
+		})
+	}
+
+	isAudio, err := utils.CheckAudioFile(audioData)
+
+	if err != nil {
+		return err
+	}
+
+	if !isAudio {
+		return c.JSON(http.StatusBadRequest, echo.Map{
+			"message": "invalid file type. please upload an audio file.",
+		})
+	}
+	
+	rawBases64, err := utils.Audio2RawBase64(audioData)
+
+	if err != nil {
+		return err
+	}
+
+	key, err := utils.RequestShazamSearchAudio(rawBases64)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, echo.Map{
+			"message": "failed to get data",
+		})
+	}
+
+	if key == "" {
+		return c.JSON(http.StatusNotFound, echo.Map{
+			"message": "song not found",
+		})
+	}
+
+	res, err := utils.RequestShazamSearchKey(key)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, echo.Map{
+			"message": "failed to get data",
+		})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"public_song_lyrics": res.GetInResponsePublicSongLyric(),
+	})
 }
